8c4bcdcc4b5b92b081c2b9dbd882e90ff52e223b
Kevin Agoh <kevinagoh@hotmail.fr>
2021-03-22 20:27:59 +0100

    form to add a dose to a cocktail created

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,4 @@
 class DosesController < ApplicationController
-
   def show
     @dose = Dose.find(params[:id])
   end
@@ -7,5 +6,17 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
